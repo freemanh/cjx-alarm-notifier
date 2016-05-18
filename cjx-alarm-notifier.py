d75ff7592b807b5ca0f526e75177198228840dcd
Freeman Han <freemanhan@icloud.com>
2016-05-18 20:23:57 +0800

    release first version

--- cjx-alarm-notifier.py
+++ cjx-alarm-notifier.py
@@ -1,6 +1,6 @@
 #!python2
 # coding: utf-8
-import urllib, calendar, md5, mysql.connector, beanstalkc, json
+import urllib, calendar, md5, mysql.connector, beanstalkc, json, time
 from datetime import datetime
 from dateutil import parser
 
@@ -14,63 +14,75 @@ def sendSMS(content, tele):
     filehandle = urllib.urlopen(smsurl.encode('GBK'))
     print('SMS web service respose code:{!s}'.format(filehandle.getcode()))
 
-    if(filehandle.getcode() == '200'):
+    status = filehandle.getcode()
+    filehandle.close()
+
+    if(status == 200):
         return True
     else:
         return False
 
-    filehandle.close()
-
 def poll():
+    print 'Start watching...'
     beanstalk = beanstalkc.Connection(host='192.168.99.100')
     for tube in ['alarm.poweroff', 'alarm.reading1', 'alarm.reading2']:
         beanstalk.watch(tube)
 
+    while True:
+        job = beanstalk.reserve(5)
+        if job is None:
+            continue
 
-    job = beanstalk.reserve(5)
-    if job is None:
-        return
+        body = json.loads(job.body)
 
-    body = json.loads(job.body)
+        addedTime = parser.parse(body['addedTime'])
 
-    addedTime = parser.parse(body['addedTime'])
-
-    tube = job.stats()['tube']
-    if(tube == 'alarm.poweroff'):
-        content = u'{:%H:%M} 设备: {!s}出现断电报警，请及时处理！'.format(addedTime, body['device'])
-    elif(tube =='alarm.reading1'):
-        content = u'传感器: {!s} 温度出现异常！当前温度: {:.1f}, 正常温度区间为:[{:.1f} - {:.1f}]'
-    elif(tube == 'alarm.reading2'):
-        content = u'传感器: {!s} 湿度出现异常！当前温度: {:.1f}, 正常温度区间为:[{:.1f} - {:.1f}]'
-    else:
-        content = None
+        tube = job.stats()['tube']
+        if(tube == 'alarm.poweroff'):
+            content = u'{:%H:%M} 设备: {!s}出现断电报警，请及时处理！'.format(addedTime, body['device'])
+        elif(tube =='alarm.reading1'):
+            content = u'{:%H:%M} 传感器: {!s} 温度出现异常！当前温度: {:.1f}, 正常温度区间为:[{:.1f} - {:.1f}]'.format(addedTime, body['sensor'], body['reading'], body['min'], body['max'])
+        elif(tube == 'alarm.reading2'):
+            content = u'{:%H:%M} 传感器: {!s} 湿度出现异常！当前湿度: {:.1f}, 正常湿度区间为:[{:.1f} - {:.1f}]'.format(addedTime, body['sensor'], body['reading'], body['min'], body['max'])
+        else:
+            content = None
 
     
-    print content.encode('utf-8')
-    succ = sendSMS(content, body['mobile'])
-    if succ:
-        job.delete()
-    else:
-        job.release(10)
+        print content.encode('utf-8')
+        succ = sendSMS(content, body['mobile'])
+        if succ:
+            job.delete()
+        else:
+            job.release(10)
+
+        time.sleep(10)
 
     beanstalk.close()
 
-def addJob():
+def addPoweroffJob():
     bt = beanstalkc.Connection(host='192.168.99.100')
     bt.use('alarm.poweroff')
     bt.put(json.dumps({'device':'test', 'mobile': '15308039727', 'addedTime': '2016-05-17T23:25:00+08:00'}))
     bt.close()
 
+def addReadingJob():
+    bt = beanstalkc.Connection(host='192.168.99.100')
+    #bt.use('alarm.reading1')
+    bt.use('alarm.reading2')
+    bt.put(json.dumps({'sensor':'test', 'mobile': '15308039727', 'reading':10.0, 'min': 15.0, 'max': 20.0, 'addedTime': '2016-05-17T23:25:00+08:00'}))
+    bt.close()
+
 def deleteJob():
     bt = beanstalkc.Connection(host='192.168.99.100')
-    bt.watch('alarm.poweroff')
+    bt.watch('alarm.reading2')
     job = bt.reserve(5)
     if job is not None:
         print job
         job.delete()
     bt.close()
-    
+
 if __name__ == '__main__':
     poll()
-    #addJob()
+    #addPoweroffJob()
+    #addReadingJob()
     #deleteJob()
